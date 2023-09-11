d0b41a3784a8c93441767071fb3692c15153d816
gheezybaby <73592196+gheezybaby@users.noreply.github.com>
2023-09-11 21:41:15 +0530

    Initial commit

---
+++ trial.java
@@ -0,0 +1,11 @@
+class Test{
+
+    public static void main(String a[]){
+
+        System.out.println("Hello World");
+        System.out.println("Second Line");
+        System.out.println("DZ Marked");
+
+    }
+    
+}
