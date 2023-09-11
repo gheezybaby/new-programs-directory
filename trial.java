8ad56957747f7d6976c12e1329a625340d98edff
gheezybaby <73592196+gheezybaby@users.noreply.github.com>
2023-09-11 22:35:08 +0530

    Update trial.java

--- trial.java
+++ trial.java
@@ -1,10 +1,10 @@
-class Test{
+class trial{
 
     public static void main(String a[]){
 
         System.out.println("Hello World");
         System.out.println("Second Line");
-        
+
         System.out.println("DZ Marked");
 
     }
