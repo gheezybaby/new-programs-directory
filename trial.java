fd329acdb4472c9f1fecf4293449ffe9188689c3
gheezybaby <73592196+gheezybaby@users.noreply.github.com>
2023-09-12 19:53:35 +0530

    Added another line

--- trial.java
+++ trial.java
@@ -3,9 +3,12 @@ class trial{
     public static void main(String a[]){
 
         System.out.println("Hello World");
+
         System.out.println("Second Line");
 
         System.out.println("DZ Marked");
+
+        System.out.println("Fourth Line");
 
     }
     
